fe8888828fc24b4ed142929e71f1691e22637025
Elfatih <elfatih.ibrahim@yahoo.com>
2020-01-16 15:53:07 +0100

    new modification

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 echo 'Bienvenue';
-?>
-<<<<<<< HEA
+
 <a href="login.php">login</a>
-=======
 <a href=”logout.php”>logout</a>
->>>>>>> ajout du lien vers le logout
+// ajout du lien vers le logout
